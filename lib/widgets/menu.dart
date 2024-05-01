3765c5259c93ce68ea237a16a75767c9f1dcdf05
Estebanreyesmi <esteban.reyes@mayor.cl>
2024-05-01 13:06:51 -0400

    v3

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:prueba2/widgets/widgets.dart';
 import 'package:prueba2/screens/screens.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -21,25 +20,65 @@ class CustomDrawer extends StatelessWidget {
               ),
             ),
           ),
-          ListTile(
-            title: Text('Ropa'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => RopaPage()),
-              );
-            },
-          ),
-          ListTile(
-            title: Text('Zapatillas'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ZapatillasPage()),
-              );
-            },
+          ExpansionTile(
+            title: Text('Vestuario'),
+            children: [
+              ListTile(
+                title: Text('Zapatillas'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RopaPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Pantalones'),
+                onTap: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => RopaPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Gorros'),
+                onTap: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => RopaPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Polerones'),
+                onTap: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => RopaPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Parkas'),
+                onTap: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => RopaPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Poleras'),
+                onTap: () {
+                  Navigator.pop(
+                    context,
+                    MaterialPageRoute(builder: (context) => RopaPage()),
+                  );
+                },
+              ),
+            ],
           ),
         ],
       ),
